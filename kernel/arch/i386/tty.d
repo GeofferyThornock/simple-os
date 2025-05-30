8f75c4e8a6926bf45f9caa3f96181f7e925a6482
Geoffery Thornock <geofferythornock@gmail.com>
2025-05-30 09:14:30 -0600

    Refactor file-structure, Add makefiles and shell scripts, fixed bugs
    This was a big update completely rewrote the file structure and learned about make files, also built my own version
    of gcc and binutils since there were some bugs with the libraries of my old version

---
+++ kernel/arch/i386/tty.d
@@ -0,0 +1,8 @@
+arch/i386/tty.o: arch/i386/tty.c \
+ /home/geoff/opt/cross/lib/gcc/i686-elf/14.3.0/include/stdbool.h \
+ /home/geoff/opt/cross/lib/gcc/i686-elf/14.3.0/include/stddef.h \
+ /home/geoff/opt/cross/lib/gcc/i686-elf/14.3.0/include/stdint.h \
+ /home/geoff/opt/cross/lib/gcc/i686-elf/14.3.0/include/stdint-gcc.h \
+ /home/geoff/projects/simple-kernel/sysroot/usr/include/string.h \
+ /home/geoff/projects/simple-kernel/sysroot/usr/include/sys/cdefs.h \
+ include/kernel/tty.h arch/i386/vga.h
---
+++ kernel/kernel/kernel.d
@@ -0,0 +1,2 @@
+kernel/kernel.o: kernel/kernel.c include/kernel/tty.h \
+ /home/geoff/opt/cross/lib/gcc/i686-elf/14.3.0/include/stddef.h
