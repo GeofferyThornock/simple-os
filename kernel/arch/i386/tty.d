732d866476b98debe6f017c26367d58bc42af32c
Geoffery Thornock <geofferythornock@gmail.com>
2025-06-02 22:33:28 -0600

    Add newline support (again)

--- kernel/arch/i386/tty.d
+++ kernel/arch/i386/tty.d
@@ -3,6 +3,6 @@ arch/i386/tty.o: arch/i386/tty.c \
  /home/geoff/opt/cross/lib/gcc/i686-elf/14.3.0/include/stddef.h \
  /home/geoff/opt/cross/lib/gcc/i686-elf/14.3.0/include/stdint.h \
  /home/geoff/opt/cross/lib/gcc/i686-elf/14.3.0/include/stdint-gcc.h \
- /home/geoff/projects/simple-kernel/sysroot/usr/include/string.h \
- /home/geoff/projects/simple-kernel/sysroot/usr/include/sys/cdefs.h \
+ /home/geoff/dev/simple-os/sysroot/usr/include/string.h \
+ /home/geoff/dev/simple-os/sysroot/usr/include/sys/cdefs.h \
  include/kernel/tty.h arch/i386/vga.h
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -3,5 +3,6 @@
 void kernel_main(void) {
 	terminal_initialize();
 	terminal_writestring("Hello, kernel World!\n");
+    terminal_writestring("testing the new \n line");
 }
 
