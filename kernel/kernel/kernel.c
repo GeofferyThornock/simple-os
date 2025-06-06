58c8c92c11b81d933b88eaa49f2ed773ec890ccd
Geoffery Thornock <geofferythornock@gmail.com>
2025-06-06 00:10:39 -0600

    Add test to print single char to terminal

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,8 +4,9 @@
 
 void kernel_main(void) {
 	terminal_initialize();
-    char test[] = "string part of print f";
+    char test[] = "string part of print f\n";
     printf("This is a test of the printf function\n");
     printf("%s", test);
+    printf("%c", 'f');
 }
 
