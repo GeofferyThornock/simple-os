537f99ac29b3babf95bb565483cf24b0ff9e9317
Geoffery Thornock <geofferythornock@gmail.com>
2025-06-16 18:28:27 -0600

    Cleaned kernel and Makefile in libc

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,9 +4,6 @@
 
 void kernel_main(void) {
 	terminal_initialize();
-    char test[] = "string part of print f\n";
     printf("This is a test of the printf function\n");
-    printf("%s", test);
-    printf("%c", 'f');
 }
 
