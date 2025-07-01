5d02af3db49f39e76294b1b6f2fba4c5655f5070
Geoffery Thornock <geofferythornock@gmail.com>
2025-07-01 14:56:13 -0600

    Fix GDT table and refactored to be inline with current project style

---
+++ kernel/arch/i386/load_gdt.asm
@@ -0,0 +1,22 @@
+global load_gdt
+
+load_gdt:
+    MOV     EAX, [ESP + 4]  ; get gdt pointer
+    LGDT    [EAX]          ; load gdt
+
+    MOV     AX, 0x10    ; kernel data segment
+    MOV     DS, AX
+    MOV     ES, AX
+    MOV     FS, AX
+    MOV     GS, AX
+    MOV     SS, AX
+
+    CLI             ; clear interrupts
+    MOV     EAX, cr0    ; set bit 0 in cr0 to enter protected mode
+    OR      EAX, 1
+    MOV	    CR0, EAX
+
+    JMP     0x08:far_jump   ; jump to far with code data segment
+far_jump:
+    ret
+
